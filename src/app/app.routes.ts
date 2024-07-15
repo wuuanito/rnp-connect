3b884c5e3ec520099baab1e8cd197b9680cc7545
Juan Frauca <juanfrauca@outlook.com>
2024-07-15 14:54:10 +0200

    Creacion layout | login (sin funcionalidad)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { LayoutComponent } from './paginas/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'layout',
+        component: LayoutComponent,
+       
+      }
+
+
+];
---
+++ src/app/paginas/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+
+
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    // Aquí implementarías la lógica de inicio de sesión
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Remember Me:', this.rememberMe);
+    // Por ejemplo, llamar a un servicio de autenticación corporativo
+  //redirigir a la página de inicio
+  this.router.navigateByUrl('layout');
+
+  }
+}
